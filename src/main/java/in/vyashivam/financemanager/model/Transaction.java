26837fcecafa258a65ec6973d92ac0590cca348c
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-09-26 20:01:35 +0530

    feat(FinanceManager): add Transaction entity:
    - Introduced Transaction entity to represent financial transactions.

---
+++ src/main/java/in/vyashivam/financemanager/model/Transaction.java
@@ -0,0 +1,37 @@
+package in.vyashivam.financemanager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "Transaction")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Sr no")
+    private Long id;
+
+    @Column(name = "Date")
+    private LocalDate date;
+
+    @Column(name = "Category")
+    private Category category;
+
+    @Column(name = "Description")
+    private String description;
+
+    @Column(name = "AmountINR", precision = 15, scale = 2)
+    private BigDecimal amount;
+
+}
