766178a4a3a41cc2735b6d52551c85a1e3a8bc0c
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-09-26 19:59:09 +0530

    feat: add Category enum for adding Category as per Transaction.

---
+++ src/main/java/in/vyashivam/financemanager/model/Category.java
@@ -0,0 +1,13 @@
+package in.vyashivam.financemanager.model;
+
+public enum Category {
+    FOOD,
+    FASHION,
+    GROCERIES,
+    MEDICINE,
+    TRANSPORTATION,
+    ENTERTAINMENT,
+    UTILITIES,
+    PERSONAL_CARE,
+    MISCELLANEOUS
+}
