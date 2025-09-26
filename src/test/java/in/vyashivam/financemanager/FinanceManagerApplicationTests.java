d2474e397b1c984266a2898d362387167ff31962
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-09-26 19:38:28 +0530

    chore: initial project setup

---
+++ src/test/java/in/vyashivam/financemanager/FinanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.vyashivam.financemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
