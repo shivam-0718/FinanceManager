a371f8c0c32a01606067e246d799e5e5fa56823f
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-09-27 14:37:10 +0530

    refactor(FinanceManager): enhance TransactionDTO with @Builder and @JsonFormat
    - Added @Builder for easier DTO construction
    - Applied @JsonFormat to date for consistent JSON serialization.

--- src/main/java/in/vyashivam/financemanager/model/TransactionDTO.java
+++ src/main/java/in/vyashivam/financemanager/model/TransactionDTO.java
@@ -1,6 +1,8 @@
 package in.vyashivam.financemanager.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
@@ -9,8 +11,12 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class TransactionDTO {
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate date;
+
     private Category category;
     private String description;
     private BigDecimal amount;
