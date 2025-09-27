a3d70de11503db86e7115f7d9c81ec59024a84c3
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-09-27 14:35:43 +0530

    feat(FinanceManager): add endpoint in TransactionController to fetch all transactions:
    - Extended TransactionController with a GET endpoint to retrieve all transactions.

--- src/main/java/in/vyashivam/financemanager/controller/TransactionController.java
+++ src/main/java/in/vyashivam/financemanager/controller/TransactionController.java
@@ -5,10 +5,8 @@ import in.vyashivam.financemanager.service.ITransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -26,4 +24,12 @@ public class TransactionController {
         String response = service.registerTransaction(transaction);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+
+    @GetMapping("")
+    public ResponseEntity<List<TransactionDTO>> getTransactions() {
+        List<TransactionDTO> transactions = service.getTransactions();
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
+
+
 }
