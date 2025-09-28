4a8b9a10d788f7cac67c54d42f5e6ed77d5d09a4
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-09-28 09:30:20 +0530

    refactor(TransactionMapper): update Mapper
    - Updated TransactionMapper static methods to include id mapping.

--- src/main/java/in/vyashivam/financemanager/mapper/TransactionMapper.java
+++ src/main/java/in/vyashivam/financemanager/mapper/TransactionMapper.java
@@ -6,6 +6,7 @@ import in.vyashivam.financemanager.model.TransactionDTO;
 public class TransactionMapper {
     public static Transaction toEntity (TransactionDTO dto) {
         return Transaction.builder()
+                .id(dto.getId())
                 .date(dto.getDate())
                 .category(dto.getCategory())
                 .description(dto.getDescription())
@@ -15,6 +16,7 @@ public class TransactionMapper {
 
     public static TransactionDTO toDto (Transaction transaction) {
         return TransactionDTO.builder()
+                .id(transaction.getId())
                 .date(transaction.getDate())
                 .category(transaction.getCategory())
                 .description(transaction.getDescription())
