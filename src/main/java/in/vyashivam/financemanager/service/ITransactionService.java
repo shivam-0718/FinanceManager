9218563bae7db125c5520a1974440fd1d01117f4
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-09-28 09:23:11 +0530

    feat(transaction): enable updating existing transactions
    - Added updateTransaction() in ITransactionService

--- src/main/java/in/vyashivam/financemanager/service/ITransactionService.java
+++ src/main/java/in/vyashivam/financemanager/service/ITransactionService.java
@@ -1,10 +1,10 @@
 package in.vyashivam.financemanager.service;
 
 import in.vyashivam.financemanager.model.TransactionDTO;
-
 import java.util.List;
 
 public interface ITransactionService {
     String registerTransaction(TransactionDTO transaction);
     List<TransactionDTO> getTransactions();
+    String updateFullTransactionDetails(TransactionDTO transaction);
 }
