0d7f40f97a4f10f807a0510ce43104aa9a4a2fef
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-09-28 11:50:22 +0530

    refactor(TransactionDto): handle validation requests by adding validations above fields.

--- src/main/java/in/vyashivam/financemanager/model/TransactionDTO.java
+++ src/main/java/in/vyashivam/financemanager/model/TransactionDTO.java
@@ -1,6 +1,7 @@
 package in.vyashivam.financemanager.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,9 +17,17 @@ public class TransactionDTO {
     private Long id;
 
     @JsonFormat(pattern = "dd-MM-yyyy")
+    @PastOrPresent(message = "Transaction date cannot be in the future.")
     private LocalDate date;
 
+    @NotNull(message = "Category is required")
     private Category category;
+
+    @NotBlank(message = "Description is required.")
+    @Size(min = 10, max = 100, message = "Description should be short.")
     private String description;
+
+    @NotNull(message = "Amount is required.")
+    @DecimalMin(value = "0.01", message = "Amount cannot be negative.")
     private BigDecimal amount;
 }
