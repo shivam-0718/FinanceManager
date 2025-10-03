0ea9f7f4c9e7e6f2d9a18743cb1afb8fb225a987
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-10-03 11:05:52 +0530

    feat(transaction-repository): add findByCategory method
    - Added findByCategory(Category category) to support category-based fetching.

--- src/main/java/in/vyashivam/financemanager/repository/TransactionRepository.java
+++ src/main/java/in/vyashivam/financemanager/repository/TransactionRepository.java
@@ -1,9 +1,14 @@
 package in.vyashivam.financemanager.repository;
 
+import in.vyashivam.financemanager.model.Category;
 import in.vyashivam.financemanager.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    //Basic filtering
+    List<Transaction> findByCategory(Category category);
 }
