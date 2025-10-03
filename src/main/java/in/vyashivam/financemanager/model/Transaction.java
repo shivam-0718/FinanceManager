c48d0abbd91691a17d796d5439e531609b4d36c9
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-10-03 11:07:22 +0530

    refactor(transaction): change ID generation strategy to IDENTITY
    - Updated @GeneratedValue strategy from AUTO to IDENTITY.
    - Aligns with database auto-increment behavior.

--- src/main/java/in/vyashivam/financemanager/model/Transaction.java
+++ src/main/java/in/vyashivam/financemanager/model/Transaction.java
@@ -18,7 +18,7 @@ import java.time.LocalDate;
 public class Transaction {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Sr no")
     private Long id;
 
