3e0ce98c1c13eef42bbbc27651c966ec347215ff
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-10-03 11:31:21 +0530

    feat(transaction-repository): add findByDateBetween method
    - Added findByDateBetween method to support fetching transactions within a date range.

--- src/main/java/in/vyashivam/financemanager/repository/TransactionRepository.java
+++ src/main/java/in/vyashivam/financemanager/repository/TransactionRepository.java
@@ -4,11 +4,16 @@ import in.vyashivam.financemanager.model.Category;
 import in.vyashivam.financemanager.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    //Basic filtering
+    //Filtering by category
     List<Transaction> findByCategory(Category category);
+
+    //Filtering based on date range
+    List<Transaction> findByDateBetween(LocalDate startDate, LocalDate endDate);
 }
