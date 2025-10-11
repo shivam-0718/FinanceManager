878bd44164252997822fbabe441760231ea48443
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-10-11 11:20:21 +0530

    feat(transaction-repo): add new method for sorting the transactions excluding deleted ones and refactor existing names

--- src/main/java/in/vyashivam/financemanager/repository/TransactionRepository.java
+++ src/main/java/in/vyashivam/financemanager/repository/TransactionRepository.java
@@ -2,18 +2,24 @@ package in.vyashivam.financemanager.repository;
 
 import in.vyashivam.financemanager.model.Category;
 import in.vyashivam.financemanager.model.Transaction;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    //Filtering by category
-    List<Transaction> findByCategory(Category category);
+    // Excluding the deleted transactions
+    List<Transaction> findByDeletedFalse();
 
-    //Filtering based on date range
-    List<Transaction> findByDateBetween(LocalDate startDate, LocalDate endDate);
+    //Filtering by category and deleted transactions
+    List<Transaction> findByCategoryAndDeletedFalse(Category category);
+
+    //Filtering based on date range and excluding deleted transactions
+    List<Transaction> findByDateBetweenAndDeletedFalse(LocalDate startDate, LocalDate endDate);
+
+    // Excluding the deleted transactions
+    List<Transaction> findByDeletedFalse(Sort sort);
 }
